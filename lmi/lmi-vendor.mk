3a1d8cb88e685ef4f0c99b0ee6471c15ae6ac683
dianlujitao <dianlujitao@lineageos.org>
2024-09-08 21:26:39 +0800

    lmi: media: Remove OMX
     * OMX HW video codecs are no longer used after enabling Codec2
     * OMX HW audio codecs won't work without QCOM A/V enhancements, which
       we don't have on AOSPA.
    
    Change-Id: I65852151c8fb6387add5fc4402ba3f13c97b5f64

--- lmi/lmi-vendor.mk
+++ lmi/lmi-vendor.mk
@@ -475,7 +475,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0-impl \
     vendor.qti.hardware.btconfigstore@2.0-impl \
     vendor.qti.hardware.fm@1.0-impl \
-    libOmxVpp \
     libadsp_default_listener \
     libadsprpc \
     libbluetooth_audio_session_qti \
